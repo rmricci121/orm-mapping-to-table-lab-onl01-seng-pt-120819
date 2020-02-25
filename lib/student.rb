caf107e341fb285f49851a2355de473397cac7de
Robert M Ricci <rmricci121@gmail.com>
2020-02-25 01:16:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,12 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-    
+    CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+        
   end
 end
